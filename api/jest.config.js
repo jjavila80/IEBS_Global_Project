e054c8bf08d882f23a2ccc09d8f0351e755e6ca9
Juan Jose Avila Lucero <jjavila@cgcom.es>
2025-06-25 09:53:02 +0200

    pipeline Global Projects

--- api/jest.config.js
+++ api/jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.js'],
   verbose: true,
-  setupFiles: ['./jest.setup.js']
+  setupFiles: ['./jest.setup.js'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
 };
-
